74aa15b551a177f1e3a2222c554862015cb4be94
Muslim <muslim@MacBook--Muslim.local>
2026-02-03 14:46:35 +0500

    fix: increase Vite chunkSizeWarningLimit

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    chunkSizeWarningLimit: 1500, // увеличьте лимит до 1500 КБ (или больше по необходимости)
   },
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
